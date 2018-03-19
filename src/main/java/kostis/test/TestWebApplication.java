fd69d4fe4c8b7a6135a3cb1c952574283923f648
Kostis Tsitsas <ktsitsas@24media.gr>
2018-03-19 23:57:43 +0200

    initial commit

---
+++ src/main/java/kostis/test/TestWebApplication.java
@@ -0,0 +1,19 @@
+package kostis.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TestWebApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TestWebApplication.class);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/test.jsp
@@ -0,0 +1,18 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Test</title>
+        <script>
+            
+        </script>
+    </head>
+    <body>
+        <form action="/test">
+            <input type="text" name="test" value="${not empty param.test?param.test:''}">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
